8d2fbee556db4f7c2b09ce45dae2136eb600a3a9
Sundara Raghav G <g.sundararaghav2005@gmail.com>
2026-03-19 19:44:49 +0530

    Time: 377 ms (93.7%), Space: 0B (100%) - LeetHub

---
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -0,0 +1,14 @@
+WITH SingleNumbers AS (
+    SELECT 
+        num 
+    FROM 
+        MyNumbers 
+    GROUP BY 
+        num 
+    HAVING 
+        COUNT(num) = 1
+)
+SELECT 
+    MAX(num) AS num 
+FROM 
+    SingleNumbers;
