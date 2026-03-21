72b0e4bb156cc89fc7ebc4bc36607b6208de94cb
Sundara Raghav G <g.sundararaghav2005@gmail.com>
2026-03-21 18:32:51 +0530

    Time: 341 ms (99.44%), Space: 0B (100%) - LeetHub

---
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select patient_id ,patient_name ,conditions
+from Patients 
+where conditions like 'DIAB1%' OR conditions like '% DIAB1%' 
+order by patient_id;
