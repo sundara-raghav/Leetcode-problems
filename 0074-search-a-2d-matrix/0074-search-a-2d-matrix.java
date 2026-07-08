340ef2b2cf03970c66c84f5020bbfca1a9dc08c0
Sundara Raghav G <g.sundararaghav2005@gmail.com>
2026-07-08 15:56:20 +0530

    Time: 0 ms (100%), Space: 43.7 MB (90.53%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -3,16 +3,13 @@ class Solution {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return false;
         }
-
-        for (int i = 0; i < matrix.length; i++) {
-            
+        for (int i = 0; i < matrix.length; i++) {   
             for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == target) {
                     return true;
                 }
             }
         }
-   
         return false;
     }
 }
